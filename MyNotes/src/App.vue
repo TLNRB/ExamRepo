55addbd9f601564b612a3952940045c01a001de1
TLNRB <nortol01@easv365.dk>
2023-10-25 16:32:56 +0200

    note store created, notes are dynamic from firebase

--- MyNotes/src/App.vue
+++ MyNotes/src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
 import { RouterView } from "vue-router";
+/*----- Store Handling -----*/
+import { useStoreNotes } from "./stores/storeNotes";
+const storeNotes = useStoreNotes();
+
+onMounted(() => {
+  storeNotes.getNotes();
+});
 </script>
 
 <template>
